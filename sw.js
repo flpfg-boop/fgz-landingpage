10cee50c7b825d38abb813a0f42007edc4db8ec8
flpfg-boop <fgztransfer.gast@gmail.com>
2026-05-12 23:09:48 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Minimal: nichts cachen, aber SW existiert -> PWA-Kriterium erfüllt
+});
